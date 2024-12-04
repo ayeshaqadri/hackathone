7783ef634e9c8255c2f92599ce90d8e96e68ffec
AYESHA QADRI <155485676+ayeshaqadri@users.noreply.github.com>
2024-12-04 15:22:37 +0500

    Update fire-base.js

--- fire-base.js
+++ fire-base.js
@@ -22,13 +22,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYtqicXERh64t6CO3eKZTWcQDGGf01Tlw",
-  authDomain: "signup-form-c125a.firebaseapp.com",
-  projectId: "signup-form-c125a",
-  storageBucket: "signup-form-c125a.firebasestorage.app",
-  messagingSenderId: "1054580943408",
-  appId: "1:1054580943408:web:99829cb273e81f12d11bf5",
-  measurementId: "G-93BQ0ZMK8B",
+  apiKey: "AIzaSyB4RWWruUA2VquqYVLR23ipUEoPIzlKFJc",
+  authDomain: "project-dc717.firebaseapp.com",
+  projectId: "project-dc717",
+  storageBucket: "project-dc717.firebasestorage.app",
+  messagingSenderId: "404723403613",
+  appId: "1:404723403613:web:fc81e5a046cc163a2136b4",
+  measurementId: "G-LGWX5ZQ714"
 };
 
 // Initialize Firebase
